e19d34813d00d7dbe0a7552ca0013ba0b268baa7
Lizewski <lukasglogan@hotmail.com>
2025-05-20 14:46:13 -0300

    Fixed

--- game.py
+++ game.py
@@ -1,16 +1,18 @@
 import json
 import random
 
-f = open("words.json", encoding="utf-8")
+f = open("database.json", encoding="utf-8")
 database = json.load(f)
 choice_c = random.choice(list(database.keys()))
-tip_c = choice_c.value()
-
+listgames = database[choice_c]
+tip_c = random.choice(listgames)
+f.close()
 
 print(f"""
     Welcome to Guess The Date!
     You have 3 attempts to guess the release date of the game.
-    {tip_c}""")
+      
+    The game is: {tip_c}""")
 
 n_choices = 3
 while n_choices > 0:
@@ -21,7 +23,8 @@ while n_choices > 0:
         break
     else:
         n_choices -= 1
-        print(f"Wrong date! Try again! You have {n_choices} attempts.")
         if n_choices == 0:
-            print("You lose! Better luck next time.")
+            print(f"You lose! Better luck next time. The answer is {choice_c}.")
             break
+        print(f"Wrong date! Try again! You have {n_choices} attempts.")
+        
